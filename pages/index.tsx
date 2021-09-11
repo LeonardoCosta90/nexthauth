740f6a5b869c27f27279421f4d73ca6e427a7563
Leonardo Costa <leonardo.costa@toodoo.com.br>
2021-09-11 14:57:39 -0300

    feat: create and apply new styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-   <div>
+   <div className={styles.container}>
      <h1>Hello World!</h1>
    </div>
   )
 }
-
-export default Home
