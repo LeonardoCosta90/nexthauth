76c9a46e22db6f191fdb553be97e99dbf0dccb99
Leonardo Costa <leonardo.costa@toodoo.com.br>
2021-09-11 16:27:28 -0300

    feat: finish project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
-import { FormEvent, useContext, useState } from 'react';
-import { AuthContext } from '../context/AuthContext';
-import styles from '../styles/Home.module.css';
+import { GetServerSideProps } from 'next';
+import { FormEvent, useContext, useState } from 'react'
+import { parseCookies } from 'nookies'
+
+import { AuthContext } from '../contexts/AuthContext';
+import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 export default function Home() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const { signIn } = useContext(AuthContext);
+  const { signIn } = useContext(AuthContext)
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
@@ -14,7 +18,7 @@ export default function Home() {
     const data = {
       email,
       password,
-    };
+    }
 
     await signIn(data);
   }
@@ -25,5 +29,11 @@ export default function Home() {
       <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
       <button type="submit">Entrar</button>
     </form>
-  );
+  )
 }
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+});
